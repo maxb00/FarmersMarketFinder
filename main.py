60e01d2116a2e867ef800f8777bfeb7848756fd9
Maxb00 <maxbar00@yahoo.com>
2016-01-28 19:05:53 -0500

    Last oomph
    I do not know how to draw the data from my tuples and format it, but the
    search feature works. I also do not have a full working list of markets.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 markets = {}
-m = open('C:/Users/barlowm/IdeaProjects/FarmersMArketFinder/Libs/markets-test.csv', 'r')
+m = open('E:/GitHub/FarmersMarketFinder/Libs/markets-test.csv', 'r')
 m.readline()
 for line in m:
     inf = line.strip().split(',')
-    markets[inf[7]] = inf[0], inf[1], inf[2], inf[3], inf[4], inf[5], inf[6]
+    markets[inf[7]] = inf[1], inf[2], inf[3], inf[4], inf[6]
 print(markets)
 try:
     searchZip = int(input("What is your zip code?"))
 except ValueError:
     print("Please enter a Zip Code.")
     exit()
+print("Data will be printed in the form of (Market Name, Website, street, city, County, State) when applicable")
+print(markets[str(searchZip)])
