b47e8f6e460f7b695ccc7930a7941544ab33b50c
Maxb00 <maxbar00@yahoo.com>
2016-02-05 08:09:30 -0500

    Finishing touches

--- main.py
+++ main.py
@@ -23,19 +23,32 @@ def index_markets(location):
             ziplist[str(info[7])].append([info[1], info[2], info[3], info[4], info[6], info[7]])
 
     return ziplist, townlist
+# Booleans for later loops
+Running = True
+FindFile = True
 
-markets, towns = index_markets(input("File Name?"))
+# Getting user input on market list
+while FindFile:
+    try:
+        markets, towns = index_markets(input("File Name?"))
+        FindFile = False
+    except FileNotFoundError:
+        print("File not found, or it is not in the right directory.")
+
+# Debugging steps, not needed.
+
+# print(markets)
+# print(towns)
 
 # Welcome
 print("Farmers Market Finder; Find your local fresh foods!")
 print("Type quit at any time to exit the program.")
 # Forever run loop.
-Running = True
 while Running:
     query = str(input("What is the name of your zip code or town?"))
     # Does the user want to quit?
     if query == "quit":
-        quit()
+        Running = False
     # Was the entry blank?
     elif query == "":
         print("No Entry.")
@@ -56,13 +69,18 @@ while Running:
                     print("Market Website:", i[1])
                     print("Location:", i[2], i[3], i[4], i[5])
                     print("-")
+            # Handling a Zip not found or incorrect
             except KeyError:
                 print("Zip Code not found. Maybe your local market is not listed?")
         # Town searching, if integer conversion failed.
         except ValueError:
             # Search by town
-            for i in towns[query]:
-                print("Market Name:", markets[i][0][0])
-                print("Market Website:", markets[i][0][1])
-                print("Location:", markets[i][0][2], markets[i][0][3], markets[i][0][4], markets[i][0][5])
-                print("-")
+            try:
+                for i in towns[query]:
+                    print("Market Name:", markets[i][0][0])
+                    print("Market Website:", markets[i][0][1])
+                    print("Location:", markets[i][0][2], markets[i][0][3], markets[i][0][4], markets[i][0][5])
+                    print("-")
+            # Handling a town that is not there or is spelled incorrectly.
+            except KeyError:
+                print("Town could not be found. Maybe your local market is not listed?")
